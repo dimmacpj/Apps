fb62c1013497dc9df49f4ea52af97fb40f9eaa1c
Neal Peng <Neal.Peng@enatel.net>
2024-10-15 16:27:07 +1300

    update

--- Python/MTDAB/CALandWF.py
+++ Python/MTDAB/CALandWF.py
@@ -9,7 +9,7 @@ def getInfo():
     opVdc = 43.2         # Output DC Voltage
     opIdc = 100           # Output DC Current
     opPower = opVdc*opIdc       # Output Power
-    turnsRatio = 10      # Turns ratio
+    turnsRatio = 10     # Turns ratio
     #Powerfactor = 1      # Set PF to 1
     inductanceL = 35*1e-6     # Pirmary inductance, unit is H
     switchFreq = 14.285*1e4     # Switching Frequency, unit is Hz
@@ -23,7 +23,7 @@ def getInfo():
     ipVamplitude = ipVphn*np.sqrt(2)      # Input Phase to Nuetral AC Voltage Amplitude
     ipIamplitude = ipIphn*np.sqrt(2)      # Input Phase to Nuetral AC Current Amplitude
     #ipACFreq = 50        # Input AC frequency
-    acAngle = 15.01         # Input AC Phase Angle, unit is degree
+    acAngle = 15.010        # Input AC Phase Angle, unit is degree
     ipVaInst = ipVamplitude*np.cos(acAngle/180*np.pi)       # Instantaneous Phase A voltage at 10 degree phase angle
     ipVbInst = ipVamplitude*np.cos(acAngle/180*np.pi + 2/3*np.pi)         # Instantaneous Phase B voltage at 10 degree phase angle
     ipVcInst = ipVamplitude*np.cos(acAngle/180*np.pi - 2/3*np.pi)         # Instantaneous Phase C voltage at 10 degree phase angle
@@ -49,7 +49,7 @@ def WFsPlot(inductanceL, switchPeriod, psPeriod, mcVMajor, mcVMinor, secVrep, ta
     fig, (axs1, axs2) = plt.subplots(2)  # Create two plots vertically in one figure 
     fig.set_size_inches(22,12)
 
-    # Create plot 1
+    # Create plot 1 for Input AC Sine Waveforms
     x1 = np.linspace(-30, 30, 60)
     phA = ipVamplitude*np.cos(x1/180*np.pi)       # Instantaneous Phase A current at 10 degree phase angle
     phB = ipVamplitude*np.cos(x1/180*np.pi + 2/3*np.pi)         # Instantaneous Phase B current at 10 degree phase angle
@@ -61,31 +61,38 @@ def WFsPlot(inductanceL, switchPeriod, psPeriod, mcVMajor, mcVMinor, secVrep, ta
     axs1.set_ylabel('Voltage (V)')
     axs1.set_title('AC Waveforms')
 
-    # Create plot 2 
+    # Create plot 2 for Primary, Secondary and Inductor voltage waveforms, and Inductor current waveform
     # Calculate data
     i1 = ((mcVMajor+secVrep)*psPeriod/(2*inductanceL))
     i2 = i1 + (mcVMajor - secVrep) * (tMajor-psPeriod) / inductanceL
     i3 = i2 + (mcVMinor - secVrep) * tMinor / inductanceL
-    priVWF = [(0, mcVMajor), (tMajor*1e6, mcVMajor), (tMajor*1e6, mcVMinor), ((tMajor+tMinor)*1e6, mcVMinor), ((tMajor+tMinor)*1e6, 0), (switchPeriod/2*1e6, 0), (switchPeriod/2*1e6, -mcVMajor), 
+    priVWF = [(0, 0), (0, mcVMajor), (tMajor*1e6, mcVMajor), (tMajor*1e6, mcVMinor), ((tMajor+tMinor)*1e6, mcVMinor), ((tMajor+tMinor)*1e6, 0), (switchPeriod/2*1e6, 0), (switchPeriod/2*1e6, -mcVMajor), 
               ((switchPeriod/2+tMajor)*1e6, -mcVMajor), ((switchPeriod/2+tMajor)*1e6, -mcVMinor), ((switchPeriod/2+tMajor+tMinor)*1e6, -mcVMinor), ((switchPeriod/2+tMajor+tMinor)*1e6, 0), 
               (switchPeriod*1e6, 0), (switchPeriod*1e6, mcVMajor)]
     secVWF = [(0, -opVdc), (psPeriod*1e6, -opVdc), (psPeriod*1e6, opVdc), ((switchPeriod/2+psPeriod)*1e6, opVdc), ((switchPeriod/2+psPeriod)*1e6, -opVdc), (switchPeriod*1e6, -opVdc)]
     priIWF = [(0, -i1), (psPeriod*1e6, i1), (tMajor*1e6, i2), ((tMajor+tMinor)*1e6, i3), (switchPeriod/2*1e6, i1), 
               ((switchPeriod/2+psPeriod)*1e6, -i1), ((switchPeriod/2+tMajor)*1e6, -i2), ((switchPeriod/2+tMajor+tMinor)*1e6, -i3), (switchPeriod*1e6, -i1)]
+    inductorVWF = [(0, 0-(-secVrep)), (0, mcVMajor-(-secVrep)), (psPeriod*1e6, mcVMajor-(-secVrep)), (psPeriod*1e6, mcVMajor-secVrep), (tMajor*1e6, mcVMajor-secVrep), (tMajor*1e6, mcVMinor-secVrep), ((tMajor+tMinor)*1e6, mcVMinor-secVrep), 
+                   ((tMajor+tMinor)*1e6, 0-secVrep), (switchPeriod/2*1e6, 0-secVrep), (switchPeriod/2*1e6, -mcVMajor-secVrep), ((switchPeriod/2+psPeriod)*1e6, -mcVMajor-secVrep), ((switchPeriod/2+psPeriod)*1e6, -mcVMajor-(-secVrep)),
+                   ((switchPeriod/2+tMajor)*1e6, -mcVMajor-(-secVrep)), ((switchPeriod/2+tMajor)*1e6, -mcVMinor-(-secVrep)), ((switchPeriod/2+tMajor+tMinor)*1e6, -mcVMinor-(-secVrep)), ((switchPeriod/2+tMajor+tMinor)*1e6, 0-(-secVrep)),
+                   (switchPeriod*1e6, 0-(-secVrep))]
+
     line1, = axs2.plot(*zip(*secVWF), color = 'tab:blue')
     line2, = axs2.plot(*zip(*priVWF), color = 'tab:orange')
+    line3, = axs2.plot(*zip(*inductorVWF), color = 'tab:red')
+
     axs2.set_xlabel('Time (s)')
     axs2.set_ylabel('Voltage (V)', color = 'tab:blue')
     axs2.tick_params(axis = 'y', labelcolor = 'tab:blue')
     axs2.set_title('Vin > Vout Positive phase')
     
     axs3 = axs2.twinx()         #   Create a new Axes with an invisible x-axis and an independent y-axis positioned opposite to the original one
-    line3, = axs3.plot(*zip(*priIWF), color = 'tab:green')
+    line4, = axs3.plot(*zip(*priIWF), color = 'tab:green')
     axs3.set_ylabel('Current (A)', color = 'tab:green')
     axs3.tick_params(axis = 'y', labelcolor = 'tab:green')
 
     #fig.tight_layout()      # Otherwise the area between the two subplots would be overlapped
-    '''
+    
     # Adjust figure to make room for the sliders
     fig.subplots_adjust(bottom = 0.3)
     # Create a horizontal slider for phase shift
@@ -93,29 +100,42 @@ def WFsPlot(inductanceL, switchPeriod, psPeriod, mcVMajor, mcVMinor, secVrep, ta
     ps_Slider = Slider(ax=axPhaseShift, label='Phase Shift (out of 0.5)', valmin= 0, valmax= 0.5, valinit=psPeriod/(switchPeriod/2)*0.5)
     
     def update(val):
+        print(ps_Slider.val)
         psPeriod = ps_Slider.val/0.5*(switchPeriod/2)
-        tMajor, tMinor, iMJAVG, iMNAVG = MTDABcal(inductanceL, switchPeriod, psPeriod, mcVMajor, mcVMinor, secVrep, targetIRatio)
+        print(psPeriod)
+        tMajor, tMinor, iMJAVG, iMNAVG = MTDABcal(inductanceL, switchPeriod, psPeriod, mcVMajor, mcVMinor, secVrep, targetIRatio, ipVaInst, ipVbInst, ipVcInst, opPower)
+        
         i1 = ((mcVMajor+secVrep)*psPeriod/(2*inductanceL))
         i2 = i1 + (mcVMajor - secVrep) * (tMajor-psPeriod) / inductanceL
         i3 = i2 + (mcVMinor - secVrep) * tMinor / inductanceL
-        priVWF = [(0, mcVMajor), (tMajor*1e6, mcVMajor), (tMajor*1e6, mcVMinor), ((tMajor+tMinor)*1e6, mcVMinor), ((tMajor+tMinor)*1e6, 0), (switchPeriod/2*1e6, 0), (switchPeriod/2*1e6, -mcVMajor), 
-                ((switchPeriod/2+tMajor)*1e6, -mcVMajor), ((switchPeriod/2+tMajor)*1e6, -mcVMinor), ((switchPeriod/2+tMajor+tMinor)*1e6, -mcVMinor), ((switchPeriod/2+tMajor+tMinor)*1e6, 0), 
-                (switchPeriod*1e6, 0), (switchPeriod*1e6, mcVMajor)]
+        priVWF = [(0, 0), (0, mcVMajor), (tMajor*1e6, mcVMajor), (tMajor*1e6, mcVMinor), ((tMajor+tMinor)*1e6, mcVMinor), ((tMajor+tMinor)*1e6, 0), (switchPeriod/2*1e6, 0), (switchPeriod/2*1e6, -mcVMajor), 
+              ((switchPeriod/2+tMajor)*1e6, -mcVMajor), ((switchPeriod/2+tMajor)*1e6, -mcVMinor), ((switchPeriod/2+tMajor+tMinor)*1e6, -mcVMinor), ((switchPeriod/2+tMajor+tMinor)*1e6, 0), 
+              (switchPeriod*1e6, 0), (switchPeriod*1e6, mcVMajor)]
         secVWF = [(0, -opVdc), (psPeriod*1e6, -opVdc), (psPeriod*1e6, opVdc), ((switchPeriod/2+psPeriod)*1e6, opVdc), ((switchPeriod/2+psPeriod)*1e6, -opVdc), (switchPeriod*1e6, -opVdc)]
         priIWF = [(0, -i1), (psPeriod*1e6, i1), (tMajor*1e6, i2), ((tMajor+tMinor)*1e6, i3), (switchPeriod/2*1e6, i1), 
-                ((switchPeriod/2+psPeriod)*1e6, -i1), ((switchPeriod/2+tMajor)*1e6, -i2), ((switchPeriod/2+tMajor+tMinor)*1e6, -i3), (switchPeriod*1e6, -i1)]
+              ((switchPeriod/2+psPeriod)*1e6, -i1), ((switchPeriod/2+tMajor)*1e6, -i2), ((switchPeriod/2+tMajor+tMinor)*1e6, -i3), (switchPeriod*1e6, -i1)]
+        inductorVWF = [(0, 0-(-secVrep)), (0, mcVMajor-(-secVrep)), (psPeriod*1e6, mcVMajor-(-secVrep)), (psPeriod*1e6, mcVMajor-secVrep), (tMajor*1e6, mcVMajor-secVrep), (tMajor*1e6, mcVMinor-secVrep), ((tMajor+tMinor)*1e6, mcVMinor-secVrep), 
+                   ((tMajor+tMinor)*1e6, 0-secVrep), (switchPeriod/2*1e6, 0-secVrep), (switchPeriod/2*1e6, -mcVMajor-secVrep), ((switchPeriod/2+psPeriod)*1e6, -mcVMajor-secVrep), ((switchPeriod/2+psPeriod)*1e6, -mcVMajor-(-secVrep)),
+                   ((switchPeriod/2+tMajor)*1e6, -mcVMajor-(-secVrep)), ((switchPeriod/2+tMajor)*1e6, -mcVMinor-(-secVrep)), ((switchPeriod/2+tMajor+tMinor)*1e6, -mcVMinor-(-secVrep)), ((switchPeriod/2+tMajor+tMinor)*1e6, 0-(-secVrep)),
+                   (switchPeriod*1e6, 0-(-secVrep))]
         line1.set_data(*zip(*secVWF))
         line2.set_data(*zip(*priVWF))
-        line3.set_data(*zip(*priIWF))
+        line3.set_data(*zip(*inductorVWF))
+        line4.set_data(*zip(*priIWF))
+        
+        print(i2)
+        axs3.set_ylim(int(-i2), int(i2))
+        axs3.margins(-2)
+        
     ps_Slider.on_changed(update)
-    '''
+    
     plt.grid(True,linestyle="-",color="gray",linewidth="0.5",axis='both')
     plt.show()
 
 def MTDABcal(inductanceL, switchPeriod, psPeriod, mcVMajor, mcVMinor, secVrep, targetIRatio, ipVaInst, ipVbInst, ipVcInst, opPower):
     """ Function to calculate the Major current and the Minor current based on knowing the ratio of the two."""
-    '''
-    Vmj, Vmn, Vs, Ll, Tps, Tmj, Tmn, Ts = sp.symbols('Vmj, Vmn, Vs, Ll, Tps, Tmj, Tmn, Ts', positive = True)
+    
+    Vmj, Vmn, Vs, Ll, Tps, Tmj, Tmn, Ts = sp.symbols('Vmj, Vmn, Vs, Ll, Tps, Tmj, Tmn, Ts', postive = True)
     i1 = (Vmj + Vs)*Tps/(2*Ll)          # Equation for instantaneous primary inductor current at the end of Tps
     i2 = i1 + (Vmj - Vs)*(Tmj - Tps)/Ll         # Equation for instantaneous primary inductor current at the end of Tjm
     i3 = i2 + (Vmn - Vs)*Tmn/Ll         # Equation for instantaneous primary inductor current at the end of Tmn
@@ -123,23 +143,30 @@ def MTDABcal(inductanceL, switchPeriod, psPeriod, mcVMajor, mcVMinor, secVrep, t
     iMJ = (i1 + i2)*(Tmj-Tps)/Ts        # Average current during major period
     iMN = (i2 + i3)*Tmn/Ts              # Average current during minor period
     
-    i1 = sp.simplify(i1.expand().subs([(Vmj, mcVMajor), (Vs, secVrep), (Tps, psPeriod), (Ll, inductanceL)]))     
+    i1 = sp.simplify(i1.expand().subs([(Vmn, mcVMinor), (Vs, secVrep), (Ll, inductanceL), (Vmj, mcVMajor), (Tps, psPeriod)]))     
+    print(i1)
+    i2 = sp.simplify(i2.expand().subs([(Vmn, mcVMinor), (Vs, secVrep), (Ll, inductanceL), (Vmj, mcVMajor), (Tps, psPeriod)]))
     i3 = sp.simplify(i3.expand().subs([(Vmn, mcVMinor), (Vs, secVrep), (Ll, inductanceL), (Vmj, mcVMajor), (Tps, psPeriod)]))        
-    
+    print(i2)
+    print(i3)
     Tzero = (i1 - i3) * inductanceL / (-secVrep) 
     periodEquation = Tmj + Tmn + Tzero - switchPeriod/2
-    actualIRatio = sp.simplify((iMN/iMJ).expand().subs([(Vmn, mcVMinor), (Vmj, mcVMajor), (Vs, secVrep), (Tps, psPeriod), (Ll, inductanceL)]))    
+    actualIRatio = sp.simplify((iMN/iMJ).expand().subs([(Vmn, mcVMinor), (Vmj, mcVMajor), (Vs, secVrep), (Tps, psPeriod), (Ll, inductanceL)]))  
+
 
     periods = sp.solve([actualIRatio - targetIRatio, periodEquation], [Tmj, Tmn])
-
-    actualImj = - iMJ.subs([(Vmn, mcVMinor), (Vmj, mcVMajor), (Vs, secVrep), (Tps, psPeriod), (Ll, inductanceL), (Ts, switchPeriod), (Tmj, periods[0][0]), (Tmn, periods[0][1])])
-    actualImn = - iMN.subs([(Vmn, mcVMinor), (Vmj, mcVMajor), (Vs, secVrep), (Tps, psPeriod), (Ll, inductanceL), (Ts, switchPeriod), (Tmj, periods[0][0]), (Tmn, periods[0][1])])
+    print(periods)
+    for i in range(len(periods)):
+        if periods[i][0] > periods[i][1]:    
+            actualImj = - iMJ.subs([(Vmn, mcVMinor), (Vmj, mcVMajor), (Vs, secVrep), (Tps, psPeriod), (Ll, inductanceL), (Ts, switchPeriod), (Tmj, periods[i][0]), (Tmn, periods[i][1])])
+            actualImn = - iMN.subs([(Vmn, mcVMinor), (Vmj, mcVMajor), (Vs, secVrep), (Tps, psPeriod), (Ll, inductanceL), (Ts, switchPeriod), (Tmj, periods[i][0]), (Tmn, periods[i][1])])
         
-    tMajor = periods[0][0]
-    tMinor = periods[0][1]
+            tMajor = periods[i][0]
+            tMinor = periods[i][1]
+    
     '''
     """ Function to calculate the Major current and the Minor current based on knowing the ratio of the two."""
-    Vmj, Vmn, Vs, Ll, Tps, Tmj, Tmn, Ts = sp.symbols('Vmj, Vmn, Vs, Ll, Tps, Tmj, Tmn, Ts', positive = True)
+    Vmj, Vmn, Vs, Ll, Tps, Tmj, Tmn, Ts = sp.symbols('Vmj, Vmn, Vs, Ll, Tps, iTmj, Tmn, Ts', positive = True)
     i1, i2, i3 = sp.symbols('i1, i2, i3')
     eq1 = ((Vmj + Vs)*Tps/(2*Ll) - i1).subs([(Vmn, mcVMinor), (Vmj, mcVMajor), (Vs, secVrep), (Tps, psPeriod), (Ll, inductanceL), (Ts, switchPeriod)])          # Equation for instantaneous primary inductor current at the end of Tps
     eq2 = (i1 + (Vmj - Vs)*(Tmj - Tps)/Ll - i2).subs([(Vmn, mcVMinor), (Vmj, mcVMajor), (Vs, secVrep), (Tps, psPeriod), (Ll, inductanceL), (Ts, switchPeriod)])         # Equation for instantaneous primary inductor current at the end of Tjm
@@ -163,7 +190,7 @@ def MTDABcal(inductanceL, switchPeriod, psPeriod, mcVMajor, mcVMinor, secVrep, t
         
     tMajor = periods[0][0]
     tMinor = periods[0][1]
-
+    '''
     return tMajor, tMinor, actualImj, actualImn
 
 def main():
--- Python/MTDAB/caltester.py
+++ Python/MTDAB/caltester.py
@@ -11,7 +11,7 @@ def main():
     inductanceL = 35*1e-6     # Pirmary inductance, unit is H
     switchFreq = 14.285*1e4     # Switching Frequency, unit is Hz
     switchPeriod = 1/switchFreq     # Switching Period, unit is s
-    phaseShiftPriSec = 2       # Phase Shift between Pri and Sec, out of 0.5, half switching cycle                                                                                                                                                                                                                                                       
+    phaseShiftPriSec = 0       # Phase Shift between Pri and Sec, out of 0.5, half switching cycle                                                                                                                                                                                                                                                       
     psPeriod = phaseShiftPriSec/0.5*(1/switchFreq/2)    # Phase Shift durition, unit is s
     ipVphph = 480        # Input Phase to Phase AC RMS Votlage
     ipIphph = opPower/1/ipVphph       # Input Phase to Phase AC RMS Current
@@ -35,43 +35,65 @@ def main():
     secVrep = opVdc*turnsRatio      # The reflect voltage at primary to replace output voltage and turns ratio
     targetIRatio = mcIMinorTarget/mcIMajorTarget        # Ratio of the target Minor current and Major current, which is equal to the ratio of the minor on time and major on time
 
-    Vmj, Vmn, Vs, Ll, Tx, Ty, Tmj, Tmn, Ts, i1, i2, i3 = sp.symbols('Vmj, Vmn, Vs, Ll, Tx, Ty, Tmj, Tmn, Ts, i1, i2, i3', positive = True)
+    #Vmj, Vmn, Vs, Ll, Tx, Ty, Tmj, Tmn, Ts, i1, i2, i3 = sp.symbols('Vmj, Vmn, Vs, Ll, Tx, Ty, Tmj, Tmn, Ts, i1, i2, i3', positive = True)
 
+    
+    Vmj, Vmn, Vs, Ll, Tps, Tmj, Tmn, Ts , Tzero = sp.symbols('Vmj, Vmn, Vs, Ll, Tps, Tmj, Tmn, Ts, Tzero', positive = True)
+    i1 = Vs*Tzero/(2*Ll)          # Equation for instantaneous primary inductor current at the end of Tps
+    i2 = i1 + (Vmj - Vs)*(Tmj)/Ll         # Equation for instantaneous primary inductor current at the end of Tjm
+    i1 = i2 + (Vmn - Vs)*Tmn/Ll         # Equation for instantaneous primary inductor current at the end of Tmn
+    Tzero = (i1 + i1) * inductanceL / (secVrep) 
+    
+
+    iMJ = (i1 + i2)*(Tmj)/Ts        # Average current during major period
+    iMN = (i2 + i1)*Tmn/Ts              # Average current during minor period
+    
+    i1 = sp.simplify(i1.expand().subs([(Vmj, mcVMajor), (Vs, secVrep), (Tps, psPeriod), (Ll, inductanceL), (Vmn, mcVMinor)]))     
+    i2 = sp.simplify(i2.expand().subs([(Vmn, mcVMinor), (Vs, secVrep), (Ll, inductanceL), (Vmj, mcVMajor), (Tps, psPeriod)]))        
+    print(i1)
+    print(i2)
+    periodEquation = Tmj + Tmn + Tzero - switchPeriod/2
+    actualIRatio = sp.simplify((iMN/iMJ).expand().subs([(Vmn, mcVMinor), (Vmj, mcVMajor), (Vs, secVrep), (Tps, psPeriod), (Ll, inductanceL)]))    
+
+    periods = sp.solve([actualIRatio - targetIRatio, periodEquation], [Tmj, Tmn])
+    print(periods[Tmj])
+    actualImj = - iMJ.subs([(Vmn, mcVMinor), (Vmj, mcVMajor), (Vs, secVrep), (Tps, psPeriod), (Ll, inductanceL), (Ts, switchPeriod), (Tmj, periods[Tmj]), (Tmn, periods[Tmn])])
+    actualImn = - iMN.subs([(Vmn, mcVMinor), (Vmj, mcVMajor), (Vs, secVrep), (Tps, psPeriod), (Ll, inductanceL), (Ts, switchPeriod), (Tmj, periods[Tmj]), (Tmn, periods[Tmn])])
+
+    print(actualImj, actualImn)   
+    tMajor = periods[Tmj]
+    tMinor = periods[Tmn]
+    #print(tMajor, tMinor)
+    
+    '''
 # Phase shift = 0
-    if phaseShiftPriSec == 0 :
-        eq1 = (- i1 -i3 + (Vmj - Vs)*Tmj/Ll).subs([(Vmn, mcVMinor), (Vs, secVrep), (Ll, inductanceL), (Vmj, mcVMajor), (Ts, switchPeriod)])          # Equation for instantaneous primary inductor current at the end of Tps
-        eq2 = (- i2 + i1 + (Vmn - Vs)*(Tmn)/Ll).subs([(Vmn, mcVMinor), (Vs, secVrep), (Ll, inductanceL), (Vmj, mcVMajor), (Ts, switchPeriod)])         # Equation for instantaneous primary inductor current at the end of Tjm
-        eq3 = (- i3 + i2 + (- Vs)*(switchPeriod/2 - Tmj - Tmn)/Ll).subs([(Vmn, mcVMinor), (Vs, secVrep), (Ll, inductanceL), (Vmj, mcVMajor), (Ts, switchPeriod)])         # Equation for instantaneous primary inductor current at the end of Tmn
-    
-# Phase shift = Tmj
-    else :
-        eq1 = (- i1 - i3 + (Vmj + Vs)*Tmj/Ll).subs([(Vmn, mcVMinor), (Vs, secVrep), (Ll, inductanceL), (Vmj, mcVMajor), (Ts, switchPeriod)])
-        eq2 = (- i2 + i1 + (Vmn - Vs)*(Tmn)/Ll).subs([(Vmn, mcVMinor), (Vs, secVrep), (Ll, inductanceL), (Vmj, mcVMajor), (Ts, switchPeriod)])
-        eq3 = (- i3 + i2 + (- Vs)*(switchPeriod/2 - Tmj - Tmn)/Ll).subs([(Vmn, mcVMinor), (Vs, secVrep), (Ll, inductanceL), (Vmj, mcVMajor), (Ts, switchPeriod)])
-    
+    Tzero = 2 * inductanceL / (secVrep)
+    eq1 = (i1 - i2 + (Vmj - Vs)*Tmj/Ll).subs([(Vmn, mcVMinor), (Vs, secVrep), (Ll, inductanceL), (Vmj, mcVMajor), (Ts, switchPeriod)])          # Equation for instantaneous primary inductor current at the end of Tps
+    eq2 = (i2 - i1 + (Vmn - Vs)*Tmn/Ll).subs([(Vmn, mcVMinor), (Vs, secVrep), (Ll, inductanceL), (Vmj, mcVMajor), (Ts, switchPeriod)])         # Equation for instantaneous primary inductor current at the end of Tjm
+    #eq3 = (- i3 + i2 + (- Vs)*(switchPeriod/2 - Tmj - Tmn)/Ll).subs([(Vmn, mcVMinor), (Vs, secVrep), (Ll, inductanceL), (Vmj, mcVMajor), (Ts, switchPeriod)])         # Equation for instantaneous primary inductor current at the end of Tmn
+    eq3 = Tzero - 2 * inductanceL / (secVrep)
+    eq5 = Tmj + Tmn + Tzero - switchPeriod/2
 # Common eqs
-    currents = sp.solve([eq1, eq2, eq3], [i1, i2, i3])
-    #print(currents[i1])
-    i1 = currents[i1]
-    i2 = currents[i2]
-    i3 = currents[i3]
-    #print(i1, i2, i3)
+    currents = sp.solve([eq1, eq2, eq3,eq5], [i1, i2])
+    print(currents)
+    i1 = currents[0]
+    i2 = currents[1]
+    #i3 = currents[i3]
+    print(i1, i2)
     
     iMJ = sp.simplify(((i1*Ty - i3*Tx)/Ts).subs([(Ts, switchPeriod)]))        # Average current during major period
     iMN = sp.simplify(((i1 + i2)*Tmn/Ts).subs([(Ts, switchPeriod)]))              # Average current during minor period
 
     #print(iMJ)
     #print(iMN/iMJ)
-
-    eq4 = Tmj - Tx - Ty
-    Tzero = (i2 - i3) * inductanceL / (secVrep) 
+    Tzero = 2 * inductanceL / (secVrep) 
     #print(Tzero)
     eq5 = Tmj + Tmn + Tzero - switchPeriod/2
     eq6 = (iMJ + iMN) * ipVaInst - iMJ * ipVbInst - iMN * ipVcInst - opPower
     actualIRatio = iMN/iMJ   
     print(ipVaInst, ipVbInst, ipVcInst)
 
-    periods = sp.solve([eq4, actualIRatio - targetIRatio, eq5, eq6], [Tmj, Tmn, Tx, Ty])
+    periods = sp.solve([actualIRatio - targetIRatio, eq5, eq6], [Tmj, Tmn])
     print(periods)
 
     actualImj = sp.simplify(iMJ.subs([(Vmn, mcVMinor), (Vmj, mcVMajor), (Vs, secVrep), (Ll, inductanceL), (Ts, switchPeriod), (Tmj, periods[0][0]), (Tmn, periods[0][1]), (Tx, periods[0][2]), (Ty, periods[0][3])]))
@@ -79,7 +101,7 @@ def main():
         
     print(actualImj)
     print(actualImn)
-    
+    '''
 
 if __name__ == '__main__':
     main()
